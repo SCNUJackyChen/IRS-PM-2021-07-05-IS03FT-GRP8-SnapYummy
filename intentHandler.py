0e05b24ef5d06736918d140e2d9ac4d900bcd2f9
mediana-medy <medy.xf@gmail.com>
2021-09-24 12:38:00 +0800

    v1

--- intentHandler.py
+++ intentHandler.py
@@ -1,12 +1,25 @@
 
 def browse_randomdish():
-	return "randomDish"
+	return "Here is your randomDish:. \nPlease type the recipename you would like to have: "
 
 def browse_byCuisineDietary(cuisineList, dietary):
 	cuisine = ' '.join([str(elem) for elem in cuisineList])
 	responsetxt = "You prefer " + cuisine + " cuisine and you have " + dietary + " constraints"
 	return responsetxt
 
+def rec_instr(recipename):
+	responsetxt = "Instructions for " + recipename + ": "
+	return responsetxt
+
+def rec_cooktime(recipename):
+	responsetxt = "Cooking time for " + recipename + ": "
+	return responsetxt
+
+def rec_allInfo(recipename):
+	instr = rec_instr(recipename)
+	cooktime = rec_cooktime(recipename)
+	responsetxt = instr + "\n" + cooktime
+	return responsetxt
 
 def Intent_Handler(intent_name, parameters):
 	if intent_name == 'Browsing - random dishes':
@@ -19,6 +32,14 @@ def Intent_Handler(intent_name, parameters):
 		dietary = parameters["dietary"]
 		# Call specific dish KG
 		response_text = browse_byCuisineDietary(cuisine, dietary)
+
+	elif intent_name == 'Recipe':
+		recipename = parameters["recipename"]
+		response_text = rec_allInfo(recipename)
+
+	elif intent_name == "Recipe - Instructions":
+		recipename = parameters["recipename"]
+		response_text = rec_instr()
 
 	else:
 		response_text = "Please try again. Unable to find a matching intent"
